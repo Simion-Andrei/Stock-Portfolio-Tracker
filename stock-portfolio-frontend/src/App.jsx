b7d0eb3ca70b2fd499356d24711c51ffefb23342
weakestlegdayenjoyer <andrei.simion32@yahoo.com>
2026-01-22 14:44:40 +0200

    Commit

--- stock-portfolio-frontend/src/App.jsx
+++ stock-portfolio-frontend/src/App.jsx
@@ -3,7 +3,6 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import DashboardPage from './pages/DashboardPage';
 
-// Componenta care protejeaza rutele (Daca nu ai token, te arunca la Login)
 const PrivateRoute = ({ children }) => {
     const token = localStorage.getItem('token');
     return token ? children : <Navigate to="/login" />;
@@ -16,14 +15,14 @@ function App() {
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/register" element={<RegisterPage />} />
 
-                {/* Ruta protejata: Dashboard */}
+                {}
                 <Route path="/dashboard" element={
                     <PrivateRoute>
                         <DashboardPage />
                     </PrivateRoute>
                 } />
 
-                {/* Orice altceva duce la dashboard (care duce la login daca n-ai token) */}
+                {}
                 <Route path="*" element={<Navigate to="/dashboard" />} />
             </Routes>
         </Router>
